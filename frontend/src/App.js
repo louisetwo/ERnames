42135c5ba56de0c2ec23715079f57be065bc24c8
Louise Ramos <louiseebss@gmail.com>
2022-06-26 18:08:56 -0300

    adjust header and update deploy command

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,13 +11,12 @@ function App() {
       </div>
       <Box
         bgcolor="WhiteSmoke"
-        //bgcolor="#484B51" ou AliceBlue
         sx={{
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          height: "calc(100vh - 64px)",
+          height: "calc(100vh - 64px - 296.5px)",
         }}
       >
         <div className="body">
